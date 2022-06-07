ff7372fbc51cbcc0d37d71d16d5b9dc8c9a913bd
Ibrahim Oluwajoba Adisa <oluwajobawilliams@gmail.com>
2022-06-06 23:20:44 -0600

    add validation form unique data validations

--- datarail/forms.py
+++ datarail/forms.py
@@ -1,7 +1,8 @@
 # Installing flask package for forms
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, BooleanField, PasswordField, SubmitField  # importing our form fields from wtforms
-from wtforms.validators import DataRequired, EqualTo, Length, Email
+from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
+from datarail.models import User
 
 # creating our registration form
 class RegistrationForm(FlaskForm):
@@ -10,6 +11,24 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        """
+        Validates that the username a user is attempting to register with does not already EXIST IN THE DATABASE
+        """
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("The username is already taken. Please choose a different username.")
+    
+    def validate_email(self, email):
+        """
+        Validates that the email a user is attempting to register with does not already EXIST IN THE DATABASE
+        """
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("The email is already taken. Please choose a different email.")
+
+
 
 # creating our login form
 class LoginForm(FlaskForm):
