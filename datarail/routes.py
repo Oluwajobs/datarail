228296a198893ce6326dbd7d0f1357c80299a64e
Ibrahim Oluwajoba Adisa <oluwajobawilliams@gmail.com>
2022-06-07 20:55:18 -0600

    redirect to next page in request

--- datarail/routes.py
+++ datarail/routes.py
@@ -1,6 +1,6 @@
 # importing dependencies
 import email
-from flask import redirect, render_template, flash, url_for
+from flask import redirect, render_template, flash, url_for, request
 from datarail.forms import RegistrationForm, LoginForm
 from datarail.models import User, posts
 from datarail import app, bcrypt, db
@@ -65,7 +65,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Login was unsuccessful. Check your information and try again", "danger")
     return render_template('login.html', title='Login', form=form)
